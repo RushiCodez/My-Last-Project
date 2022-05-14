dd403a62e243bd119057443cb92b1ef33fe48007
RushiCodez <94935989+RushiCodez@users.noreply.github.com>
2022-05-14 11:56:11 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ camera = document.getElementById("camera");
 
 Webcam.attach("#camera");
 
-function take_snapshot(){
+function takeSnapshot(){
 Webcam.snap(function (data_uri){
 document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'"/>';
 
